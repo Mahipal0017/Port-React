66f8fe1579a581de197b3aae1ff5d463b0ad1464
Mahi <Mahi@Sindhujas-MacBook-Pro.local>
2020-09-03 21:57:58 -0400

    work

--- src/components/work.js
+++ src/components/work.js
@@ -35,16 +35,16 @@ class Work extends Component{
                         </Col>
                     </Row>
                     {/* <Media>
-      <Media left href="#">
-        <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
-      </Media>
-      <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </Media>
-    </Media> */}
+                      <Media left href="#">
+                        <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+                      </Media>
+                      <Media body>
+                        <Media heading>
+                          Media heading
+                        </Media>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                      </Media>
+                    </Media> */}
                     <Row className="be-center w-75 animated5 fadeIn">
                         <WorkItem />
                         {/* <WorkCommentItem /> */}
